24cd2d1535260abfec75fd776ec8c7af406b0836
Ninte <ninte.dev@gmail.com>
2022-02-03 18:16:36 +0100

    Setup component system & and basic output display functionality.

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Divider from './Divider';
+import Sentiment from './Sentiment';
 
-export default function Feedback() {
+export default function Feedback(props) {
+  const [feedback, setFeedback] = useState('');
+  const [rating, setRating] = useState(0);
+  const [outputVisibility, setOutputVisibility] = useState(false);
+
   return (
     <div>
+      <Divider />
+
+      {/* Buttons */}
+      <div className="mt-2 d-flex justify-content-between">
+        <button
+          className="btn btn-warning btn-md"
+          onClick={() => setOutputVisibility(true)}>
+          Review <i className="bi bi-box-arrow-in-right"></i>
+        </button>
+        <button
+          className="btn btn-danger btn-md"
+          onClick={() => setOutputVisibility(false)}>
+          Close <i className="bi bi-x-circle-fill"></i>
+        </button>
+      </div>
+
       <Divider />
 
       {/* <!--   textarea     --> */}
@@ -16,8 +37,92 @@ export default function Feedback() {
         <textarea
           className="form-control w-75"
           id="feedbackForm"
+          value={feedback}
+          onChange={(e) => setFeedback(e.target.value)}
           rows="3"></textarea>
       </div>
+
+      <Divider />
+
+      {/* <!--   stars     --> */}
+      <div className="mt-2">
+        <h4 className="teal">Rating</h4>
+        <div className="d-flex">
+          <div className="form-check pe-3">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="rating1"
+              value={1}
+              onClick={(e) => setRating(e.target.value)}
+            />
+            <label className="form-check-label" for="rating1">
+              1 <i className="bi bi-star-fill orange"></i>
+            </label>
+          </div>
+          <div className="form-check pe-3">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="rating2"
+              value={2}
+              onClick={(e) => setRating(e.target.value)}
+            />
+            <label className="form-check-label" for="rating2">
+              2 <i className="bi bi-star-fill orange"></i>
+            </label>
+          </div>
+          <div className="form-check pe-3">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="rating3"
+              value={3}
+              onClick={(e) => setRating(e.target.value)}
+            />
+            <label className="form-check-label" for="rating3">
+              3 <i className="bi bi-star-fill orange"></i>
+            </label>
+          </div>
+          <div className="form-check pe-3">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="rating4"
+              value={4}
+              onClick={(e) => setRating(e.target.value)}
+            />
+            <label className="form-check-label" for="rating4">
+              4 <i className="bi bi-star-fill orange"></i>
+            </label>
+          </div>
+          <div className="form-check pe-3">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="rating5"
+              value={5}
+              onClick={(e) => setRating(e.target.value)}
+            />
+            <label className="form-check-label" for="rating5">
+              5 <i className="bi bi-star-fill orange"></i>
+            </label>
+          </div>
+        </div>
+      </div>
+
+      {/* Sentiment Analysis */}
+      <Sentiment
+        status={'P/N'}
+        weighting={'xx.xx'}
+        rating={rating}
+        visible={outputVisibility}
+      />
     </div>
   );
 }
--- src/components/Sentiment.js
+++ src/components/Sentiment.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Divider from './Divider';
 
-export default function Sentiment() {
+export default function Sentiment(props) {
   return (
     <div>
       <Divider />
 
       {/* <!--    sentiment table    --> */}
-      <table className="table table-striped">
-        <thead>
-          <th className="table-info w-25">
-            <h4>Status</h4>
-          </th>
-          <th className="table-info w-25">
-            <h4>Weighting</h4>
-          </th>
-          <th className="table-info w-25">
-            <h4>Rating</h4>
-          </th>
-        </thead>
-        <tbody>
-          <td className="table-primary w-25">
-            <p className="ps-2">Positive / Negative</p>
-          </td>
-          <td className="table-primary w-25">
-            <p className="ps-2">XX.XX %</p>
-          </td>
-          <td className="table-primary w-25">
-            <p className="ps-2">X star</p>
-          </td>
-        </tbody>
-      </table>
+      {props.visible ? (
+        <table className="table table-striped">
+          <thead>
+            <th className="table-info w-25">
+              <h4>Status</h4>
+            </th>
+            <th className="table-info w-25">
+              <h4>Weighting</h4>
+            </th>
+            <th className="table-info w-25">
+              <h4>Rating</h4>
+            </th>
+          </thead>
+          <tbody>
+            <td className="table-primary w-25">
+              <p className="ps-2">{props.status}</p>
+            </td>
+            <td className="table-primary w-25">
+              <p className="ps-2">{props.weighting} %</p>
+            </td>
+            <td className="table-primary w-25">
+              <p className="ps-2">{props.rating} star</p>
+            </td>
+          </tbody>
+        </table>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
